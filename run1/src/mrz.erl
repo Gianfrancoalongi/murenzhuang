da2188fcf76fb7a241d44fca9904da8ef2b92e31
Gianfranco Alongi <egiaalo@esekiws5098.rnd.ki.sw.ericsson.se>
2012-12-03 16:40:12 +0100

    Fixed mistake

--- run1/src/mrz.erl
+++ run1/src/mrz.erl
@@ -4,7 +4,7 @@
 run(Options) ->    
     Lookup = [{[],fun mrz_transform:id/1},
 	      {[rev2],fun mrz_transform:rev2/1},
-	      {[rev2H],fun mrz_transform:rev2/1},
+	      {[rev2H],fun mrz_transform:rev2H/1},
 	      {[cap2H],fun mrz_transform:cap2H/1}
 	     ],
     Fun = proplists:get_value(Options,Lookup),
