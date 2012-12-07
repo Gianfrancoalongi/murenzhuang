5da0562249fce64b7bb112e797ac48ff96a342cb
Gianfranco Alongi <egiaalo@esekiws5098.rnd.ki.sw.ericsson.se>
2012-12-07 15:18:44 +0100

    Removed capHT feature

--- run1/src/o_plugin_capHT.erl
+++
@@ -1,7 +0,0 @@
--module(o_plugin_capHT).
--export([act/1]).
-act(Input) ->
-    [H|T] = string:tokens(Input," "),
-    [TH|TT] = lists:reverse(T),
-    string:join([string:to_upper(H)]++TT++[string:to_upper(TH)]," ").
-
