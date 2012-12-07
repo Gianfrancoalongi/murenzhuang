d0c41ea562852887bfe3efcac6696d1812441d90
Gianfranco Alongi <egiaalo@esekiws5098.rnd.ki.sw.ericsson.se>
2012-12-07 14:26:08 +0100

    Removed revwhole feature

--- run1/src/o_plugin_revwhole.erl
+++
@@ -1,5 +0,0 @@
--module(o_plugin_revwhole).
--export([act/1]).
-act(Input) ->
-    lists:reverse(Input).
-
--- run1/test/mrz_tests.erl
+++ run1/test/mrz_tests.erl
@@ -8,7 +8,6 @@ mrz_options_test_() ->
      [
       fun basic_program_test_function/1,
       fun rev2_test_function/1,
-      fun revwhole_test_function/1,
       fun capHT_test_function/1,
       fun lowerwhole_test_function/1,
       fun lower2_test_function/1
@@ -36,14 +35,6 @@ capHT_test_function(FileHandle) ->
 	    mrz:run([helloworld,capHT,stdout]),
 	    {ok,Res} = file:read_file("test_res.txt"),
 	    ?assertEqual(<<"HELLO WORLD">>,Res)
-    end.
-
-revwhole_test_function(FileHandle) ->
-    fun() ->
-	    group_leader(FileHandle,self()),
-	    mrz:run([helloworld,revwhole,stdout]),
-	    {ok,Res} = file:read_file("test_res.txt"),
-	    ?assertEqual(<<"dlrow olleh">>,Res)
     end.
 
 rev2_test_function(FileHandle) ->
