b5738c5119e4f20c0719e813c89d5536425cfe4f
Gianfranco Alongi <egiaalo@esekiws5098.rnd.ki.sw.ericsson.se>
2013-03-20 15:27:06 +0100

    Fixing indentation and brace placement for Emacs JS mode

--- engine.js
+++ engine.js
@@ -37,13 +37,11 @@ function add_mutator_randomly_on_edge()
 
 function add_output_node_randomly_from_input_node_or_mutator_node()
 {
-    if (Math.round(Math.random()) == 0)
-	{
-	    add_output_node_from_input();
-	}else
-	{
-	    add_output_node_from_random_mutator_node();
-	}
+    if (Math.round(Math.random()) == 0) {
+	add_output_node_from_input();
+    }else{
+	add_output_node_from_random_mutator_node();
+    }
 }
  
 function add_output_node_from_random_mutator_node()
@@ -67,18 +65,17 @@ function add_node_between(source,new_node,end)
 function create_new_mutator()
 {
     var name=random_mutator_name();
-    while( mutator_name_is_used(name) )
-	{
-	    name=random_mutator_name();
-	}
+    while( mutator_name_is_used(name) ) {
+	name=random_mutator_name();
+    }
     mutators.push(name);
     return name;           
 }
 
 function mutator_name_is_used(name)
 {
-    return $.inArray(name, mutators) != -1
-	}
+    return $.inArray(name, mutators) != -1;
+}
 
 function random_mutator_name()
 {
@@ -88,18 +85,17 @@ function random_mutator_name()
 function create_new_output_file()
 {
     var file_name=random_file_name();
-    while( file_name_is_used(file_name) )
-	{
-	    file_name=random_file_name();
-	}
+    while( file_name_is_used(file_name) ) {
+	file_name=random_file_name();
+    }
     files.push(file_name);
     return file_name;
 }      
 
 function file_name_is_used(file_name)
 {
-    return $.inArray(file_name, files) != -1
-	}
+    return $.inArray(file_name, files) != -1;
+}
 
 function random_file_name()
 {
@@ -110,10 +106,9 @@ function create_output_shape_dot_code()
 {
     var output="";
     var length = files.length, element = null;
-    for(var i = 0; i < length; i++)
-	{
-	    output+=files[i]+'[shape="note"];'
-		}
+    for(var i = 0; i < length; i++) {
+	output+=files[i]+'[shape="note"];';
+    }
     return output;
 }
 
@@ -121,21 +116,18 @@ function create_dot_code_from_paths()
 {          
     var length = paths.length, element = null;
     var dot_paths=[];
-    for (var i = 0; i < length; i++)
-	{
-	    element = paths[i];
-	    var parts = element.length, subpart = null;
-	    for (var j = 1; j < parts; j++)
-		{
-		    dot_paths.push(element[j-1]+"->"+element[j]);
-		}
+    for (var i = 0; i < length; i++) {
+	element = paths[i];
+	var parts = element.length, subpart = null;
+	for (var j = 1; j < parts; j++) {
+	    dot_paths.push(element[j-1]+"->"+element[j]);
 	}
+    }
     var dot_code="";
     var length = dot_paths.length, element = null;
-    for (var i = 0; i < length; i++)
-	{
-	    dot_code+=dot_paths[i]+";";
-	}
+    for (var i = 0; i < length; i++) {
+	dot_code+=dot_paths[i]+";";
+    }
     return dot_code;
 }
 
