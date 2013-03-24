8dd3e99ed66a001f838336cf4606afd44681b852
Gianfrancoalongi <gianfranco.alongi@gmail.com>
2013-03-24 15:51:39 +0100

    Not using two graphs. Using a 'class' to contain the data in one object.

--- engine.js
+++ engine.js
@@ -4,190 +4,277 @@ var ADD_MUTATOR='add_mutator';
 var ADD_OUTPUT='add_output';
 var REMOVE_NODE='remove_node';
 
-var paths;
-var files;
-var mutators;
+function new_graph(img_id) 
+{ 
+    var graph = {
+	paths: [[STDIN,STDOUT]],
+	files: [],
+	mutators: [],
+	img_id: img_id,
+	newly_added_path:[],
+	newly_removed_path:[],
 
-function initialize()
-{
-    files=[];
-    paths=[];
-    mutators=[];
-    paths.push([STDIN,STDOUT]);
-}
+	next_step_and_draw: function() {
+	    this.next_step();
+	    this.make_graphviz_graph();
+	},
 
-function next_step()
-{
-    var actions = [ADD_MUTATOR,ADD_OUTPUT,REMOVE_NODE];
-    var chosen_action = choose_one_randomly(actions);
-    next_step_based_on_action(chosen_action);
-}
+	done_and_draw: function() {
+	    this.done();
+	    this.make_graphviz_graph();
+	},
 
-function next_step_based_on_action(action) 
-{
-    console.log(action);
-    switch(action) {
-    case ADD_MUTATOR:
-	add_mutator_randomly_on_edge();
-	break;
-    case ADD_OUTPUT:
-	add_output_node_randomly_from_input_node_or_mutator_node();
-	break;
-    case REMOVE_NODE:
-	remove_node_randomly();
-	break;
-    }
-}
+	done: function() {
+	    console.log('do something with newly added or deleted');
+	    this.newly_added_path=[];
+	    this.newly_removed_path=[];
+	},
 
-function add_output_node_from_input()
-{
-    var output_file=create_new_output_file();
-    add_node_from(STDIN,output_file);
-}
+	add_mutator_randomly_on_edge: function() {
+	    var mutator = this.create_new_mutator();
+	    var chosen = this.randomly_choose_existing_path();
+	    var point = Math.floor(Math.random()*(chosen.length-1))+1;
+	    var new_edge = copy_edge(chosen);
+	    new_edge.splice(point,0,mutator);
+	    this.paths.push(new_edge);
+	},
 
-function add_mutator_randomly_on_edge()
-{
-    var mutator = create_new_mutator();
-    var chosen = randomly_choose_existing_path();
-    var point = Math.floor(Math.random()*(chosen.length-1))+1;
-    var new_edge = copy_edge(chosen);
-    new_edge.splice(point,0,mutator);
-    paths.push(new_edge);
-}
+	create_new_mutator: function() {
+	    var name = random_mutator_name();
+	    while( this.mutator_name_is_used(name) ) {
+		name = random_mutator_name();
+	    }
+	    this.mutators.push(name);
+	    return name;           
+	},
 
-function add_output_node_randomly_from_input_node_or_mutator_node()
-{
-    var possibles=['stdin'];
-    if (there_are_any_mutator_nodes()) {
-	possibles.push('mutator');
-    }
-    var chosen=choose_one_randomly(possibles);
-    if ( chosen == 'stdin' ) {
-	add_output_node_from_input();
-    }else{
-	add_output_node_from_random_mutator_node();
-    }
-}
+	mutator_name_is_used: function(name) {
+	    return $.inArray(name, this.mutators) != -1;
+	},
 
-function remove_node_randomly()
-{
-    var possibles=determine_possible_to_remove();
-    if (possibles.length > 0) {
-	var chosen=choose_one_randomly(possibles);
-	if ( chosen == 'output' ) {
-	    remove_random_output_node();
-	}else{
-	    remove_random_mutator_node();
+	create_dot_code_from_paths: function() {          
+	    var length = this.paths.length, element = null;
+	    var dot_paths = [];
+	    for (var i = 0; i < length; i++) {
+		element = this.paths[i];
+		var parts = element.length;
+		for (var j = 1; j < parts; j++) {
+		    dot_paths.push(element[j-1]+"->"+element[j]);
+		}
+	    }
+	    return dot_paths.join(";");
+	},
+	
+	create_output_shape_dot_code: function() {
+	    return this.files.join('[shape="note"];')+'[shape="note"];';
+	},
+
+	file_name_is_used: function(file_name) {
+	    return $.inArray(file_name, this.files) != -1;
+	},
+
+	create_new_output_file: function() {
+	    var file_name = random_file_name();
+	    while( this.file_name_is_used(file_name) ) {
+		file_name = random_file_name();
+	    }
+	    this.files.push(file_name);
+	    return file_name;
+	},
+
+	add_node_from: function(source,new_node) {
+	    this.paths.push([source,new_node]);
+	},
+
+	choose_random_mutator: function() {
+	    return choose_one_randomly(this.mutators);
+	},
+	
+	add_output_node_from_random_mutator_node: function() {
+	    var chosen=this.choose_random_mutator();
+	    var path_indices=[];
+	    var len=this.paths.length;
+	    for(var i=0; i< len; i++) {
+		var index=this.paths[i].indexOf(chosen);
+		if ( index != -1 ) {
+		    path_indices.push(i);
+		}
+	    }
+	    var path_index = choose_one_randomly(path_indices);
+	    var copy = copy_edge(this.paths[path_index]);
+	    var output_file = this.create_new_output_file(); 
+	    var mutator_index = copy.indexOf(chosen);
+	    copy.splice(mutator_index+1,0,output_file);
+	    copy.splice(mutator_index+2,copy.length-mutator_index-2);
+	    this.paths.push(copy);
+	},
+
+	randomly_choose_existing_path: function() {
+	    return choose_one_randomly(this.paths);
+	},
+	
+	remove_chosen_from_files: function(chosen) {
+	    var index=this.files.indexOf(chosen);
+	    this.files.splice(index,1);
+	},
+	
+	pick_random_output_node: function() {
+	    return choose_one_randomly(this.files);
+	},
+
+	remove_random_output_node: function() { 
+	    var chosen=this.pick_random_output_node();
+	    this.remove_path_with_output_node(chosen);
+	    this.remove_chosen_from_files(chosen);
+	},
+
+	remove_chosen_node_from_mutators: function(chosen) {
+	    var index=this.mutators.indexOf(chosen);
+	    this.mutators.splice(index,1);
+	},
+
+	path_is_dummy_path: function(index) {
+	    return ((this.paths[index][0] == STDIN) && 
+		    (this.paths[index][1] == STDOUT) && 
+		    (this.paths[index].length == 2));
+	},
+	
+	path_is_single_node: function(index) {
+	    return (this.paths[index].length == 1);
+	},
+
+	path_should_be_removed: function(index) {
+	    return (this.path_is_single_node(index) || 
+		    this.path_is_dummy_path(index));
+	},
+	
+	remove_chosen_node_from_all_paths: function(chosen) { 
+	    var len=this.paths.length;
+	    while(len--) {
+		var i=len;
+		var index=this.paths[i].indexOf(chosen);
+		if ( index != -1 ) {
+		    this.paths[i].splice(index,1);
+		    if ( this.path_should_be_removed(i) ) { 
+			this.paths.splice(i,1);
+		    }
+		}
+	    }
+	},
+	
+	remove_path_with_output_node: function(chosen) {  
+	    var len=this.paths.length;
+	    while(len--) {
+		var index=this.paths[len].indexOf(chosen);
+		if ( index != -1 ) {
+		    this.paths.splice(len,1);
+		}
+	    }
+	},
+	
+	pick_random_mutator_node: function() {
+	    return choose_one_randomly(this.mutators);
+	},
+	
+	remove_random_mutator_node: function() {
+	    var chosen=this.pick_random_mutator_node();
+	    this.remove_chosen_node_from_all_paths(chosen);
+	    this.remove_chosen_node_from_mutators(chosen);
+	},
+
+	there_are_any_output_nodes: function() {
+	    return (this.files.length > 0);
+	},
+	
+	there_are_any_mutator_nodes: function() {
+	    return (this.mutators.length > 0);
+	},
+
+	determine_possible_to_remove: function() {
+	    var possibles=[];
+	    if (this.there_are_any_output_nodes()) {
+		possibles.push('output');
+	    }
+	    if (this.there_are_any_mutator_nodes()) {
+		possibles.push('mutator');
+	    }
+	    return possibles;
+	},
+
+	remove_node_randomly: function() {
+	    var possibles=this.determine_possible_to_remove();
+	    if (possibles.length > 0) {
+		var chosen=choose_one_randomly(possibles);
+		if ( chosen == 'output' ) {
+		    this.remove_random_output_node();
+		}else{
+		    this.remove_random_mutator_node();
+		}
+	    }
+	},
+
+	add_output_node_randomly_from_input_node_or_mutator_node: function() {
+	    var possibles=['stdin'];
+	    if (this.there_are_any_mutator_nodes()) {
+		possibles.push('mutator');
+	    }
+	    var chosen=choose_one_randomly(possibles);
+	    if ( chosen == 'stdin' ) {
+		this.add_output_node_from_input();
+	    }else{
+		this.add_output_node_from_random_mutator_node();
+	    }
+	},
+	
+	add_output_node_from_input: function() {
+	    this.add_node_from(STDIN,
+			       this.create_new_output_file()
+			      );
+	},
+
+	next_step_based_on_action: function(action) {
+	    console.log(action);
+	    switch(action) {
+	    case ADD_MUTATOR:
+		this.add_mutator_randomly_on_edge();
+		break;
+	    case ADD_OUTPUT:
+		this.add_output_node_randomly_from_input_node_or_mutator_node();
+		break;
+	    case REMOVE_NODE:
+		this.remove_node_randomly();
+		break;
+	    }
+	},
+
+	next_step: function() {
+	    var actions = [ADD_MUTATOR,ADD_OUTPUT,REMOVE_NODE];
+	    var chosen_action = choose_one_randomly(actions);
+	    this.next_step_based_on_action(chosen_action);	    
+	},
+	
+	make_graphviz_graph: function()
+	{
+	    var input_output = create_edge_shape_dot_code();
+	    var output_shapes = this.create_output_shape_dot_code();
+	    var path_dot_code = this.create_dot_code_from_paths();
+	    var dot_code = 'strict digraph gr{ ' + input_output + output_shapes + path_dot_code + ' }';
+	    var options = {cht: "gv", chl: dot_code };
+	    var request = "https://chart.googleapis.com/chart?"+$.param(options);
+	    $('#'+this.img_id).attr('src',request);
 	}
-    }
+
+    };
+    return graph;
 }
+
+var graph = new_graph('graph_img');
 
 function choose_one_randomly(possibles) 
 {
     var max=possibles.length - 1;
     var index=Math.round(Math.random()*max);
     return possibles[index];
-}
-
-function determine_possible_to_remove()
-{
-    var possibles=[];
-    if (there_are_any_output_nodes()) {
-	possibles.push('output');
-    }
-    if (there_are_any_mutator_nodes()) {
-	possibles.push('mutator');
-    }
-    return possibles;
-}
-
-function there_are_any_output_nodes() 
-{
-    return (files.length > 0);
-}
-
-function there_are_any_mutator_nodes()
-{
-    return (mutators.length > 0);
-}
-
-function remove_random_mutator_node()
-{
-    var chosen=pick_random_mutator_node();
-    remove_chosen_node_from_all_paths(chosen);
-    remove_chosen_node_from_mutators(chosen);
-}
-
-function pick_random_mutator_node()
-{
-    var index=Math.round((mutators.length-1)*Math.random());
-    return mutators[index];
-}
-
-function remove_path_with_output_node(chosen) 
-{  
-    var len=paths.length;
-    while(len--) {
-	var index=paths[len].indexOf(chosen);
-	if ( index != -1 ) {
-	    paths.splice(len,1);
-	}
-    }
-}
-
-function remove_chosen_node_from_all_paths(chosen) 
-{ 
-    var len=paths.length;
-    while(len--) {
-	var i=len;
-	var index=paths[i].indexOf(chosen);
-	if ( index != -1 ) {
-	    paths[i].splice(index,1);
-	    if ( path_should_be_removed(i) ) { 
-		paths.splice(i,1);
-	    }
-	}
-    }
-}
-
-function path_should_be_removed(index) {
-    return (path_is_single_node(index) || 
-	    path_is_dummy_path(index));
-}
-
-function path_is_single_node(index) {
-    return (paths[index].length == 1);
-}
-
-function path_is_dummy_path(index) {
-    return ((paths[index][0] == STDIN) && 
-	    (paths[index][1] == STDOUT) && 
-	    (paths[index].length == 2));
-}
-
-function remove_chosen_node_from_mutators(chosen)
-{
-    var index=mutators.indexOf(chosen);
-    mutators.splice(index,1);
-}
-
-function remove_random_output_node() 
-{ 
-    var chosen=pick_random_output_node();
-    remove_path_with_output_node(chosen);
-    remove_chosen_from_files(chosen);
-}
-
-function pick_random_output_node()
-{
-    var index=Math.round((files.length-1)*Math.random());
-    return files[index];
-}
-
-function remove_chosen_from_files(chosen)
-{
-    var index=files.indexOf(chosen);
-    files.splice(index,1);
 }
 
 function copy_edge(chosen)
@@ -199,79 +286,9 @@ function copy_edge(chosen)
     return new_edge;
 }
 
-function randomly_choose_existing_path()
-{
-    return choose_one_randomly(paths);
-}
- 
-function add_output_node_from_random_mutator_node()
-{
-    var chosen=choose_random_mutator();
-    var path_indices=[];
-    var len=paths.length;
-    for(var i=0; i< len; i++) {
-	var index=paths[i].indexOf(chosen);
-	if ( index != -1 ) {
-	    path_indices.push(i);
-	}
-    }
-    var path_index=path_indices[ Math.round(Math.random()*(path_indices.length-1)) ];
-    var copy=copy_edge(paths[path_index]);
-    var output_file=create_new_output_file(); 
-    var mutator_index=copy.indexOf(chosen);
-    copy.splice(mutator_index+1,0,output_file);
-    copy.splice(mutator_index+2,copy.length-mutator_index-2);
-    paths.push(copy);
-}
-
-function choose_random_mutator()
-{
-    return choose_one_randomly(mutators);
-}
-
-function add_node_from(source,new_node)
-{
-    paths.push([source,new_node]);
-}
- 
-function add_node_between(source,new_node,end)
-{
-    paths.push([source,new_node,end]);
-}
-
-function create_new_mutator()
-{
-    var name=random_mutator_name();
-    while( mutator_name_is_used(name) ) {
-	name=random_mutator_name();
-    }
-    mutators.push(name);
-    return name;           
-}
-
-function mutator_name_is_used(name)
-{
-    return $.inArray(name, mutators) != -1;
-}
-
 function random_mutator_name()
 {
     return "mutator_"+Math.floor((Math.random()*100)+1);
-}
-
-function create_new_output_file()
-{
-    var file_name=random_file_name();
-    while( file_name_is_used(file_name) ) {
-	file_name=random_file_name();
-    }
-    files.push(file_name);
-    return file_name;
-}      
-
-function file_name_is_used(file_name)
-{
-    return $.inArray(file_name, files) != -1;
 }
 
 function random_file_name()
@@ -279,37 +296,7 @@ function random_file_name()
     return "file_"+Math.floor((Math.random()*100)+1);
 }
 
-function create_output_shape_dot_code()
-{
-    return files.join('[shape="note"];')+'[shape="note"];';
-}
-
 function create_edge_shape_dot_code()
 {
     return (STDIN+'[shape="house"];'+STDOUT+'[shape="invhouse"];')
-}
-
-function create_dot_code_from_paths()
-{          
-    var length = paths.length, element = null;
-    var dot_paths = [];
-    for (var i = 0; i < length; i++) {
-	element = paths[i];
-	var parts = element.length;
-	for (var j = 1; j < parts; j++) {
-	    dot_paths.push(element[j-1]+"->"+element[j]);
-	}
-    }
-    return dot_paths.join(";");
-}
-
-function make_graphviz_graph()
-{
-    var input_output = create_edge_shape_dot_code();
-    var output_shapes = create_output_shape_dot_code();
-    var path_dot_code = create_dot_code_from_paths();
-    var dot_code = 'strict digraph gr{ ' + input_output + output_shapes + path_dot_code + ' }';
-    var options = {cht: "gv", chl: dot_code };
-    var request = "https://chart.googleapis.com/chart?"+$.param(options);
-    $('#graph_img').attr('src',request);
 }
