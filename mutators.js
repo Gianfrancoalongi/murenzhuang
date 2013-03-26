4a521e4e4a70c83378f4f58fd865fd16f8b29c85
Gianfranco Alongi <egiaalo@esekiws5098.rnd.ki.sw.ericsson.se>
2013-03-26 16:24:29 +0100

    Adding more functionality

--- mutators.js
+++ mutators.js
@@ -7,12 +7,49 @@ function new_mutator(id) {
 	arguments: this.random_arguments(),
 
 	random_transform: function() {
-	    
+	    return choose_one_randomly(["order",
+					"increase",
+					"decrease",
+					"type",
+					"copy",
+					"sum",
+					"encode"]);
 	}
+
+	random_arguments: function() {
+	    var upto = this.buffer + 1;
+	    var args = shuffle(sequence(1,upto));
+	    remove_some(args);
+	    return args;
+	}
+	
     };
     return mutator;
 }
 
 function random_buffer_size() {
     Math.round(Math.random()*7)+1;
+}
+
+function shuffle(o){
+    for(var j, x, i = o.length; i; j= parseInt(Math.random() * i), 
+	x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+};
+
+function sequence(from,to) {
+    var nums=[];
+    for(var i = from; i <= to; i++) {
+	nums.push(i);
+    }
+    return sequence;
+}
+
+function remove_some(array) {
+    var len = array.length;
+    wile( len-- ) {
+	if ( Math.round(Math.random()) == 0 ) {
+	    array.splice(len,1);
+	}
+    }
 }
