d9d8e6efccf3a9e1ea804d6d6e8c5ad941a3564b
Gianfrancoalongi <gianfranco.alongi@gmail.com>
2013-05-15 21:59:18 +0200

    C# skeleton - courtesy of Kim Honkaniemi

---
+++ skeletons/c#/Std/StdLibrary/Std.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StdLibrary
+{
+    public class Std
+    {
+        public static void input(string input)
+        {
+            Console.Write(input);
+        }
+    }
+}
---
+++ skeletons/c#/Std/TestProject/StdTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using StdLibrary;
+
+[TestClass]
+public class Std_Tests
+{
+    [TestMethod]
+    public void StdTest1()
+    {
+        using (var writer = new StringWriter(new StringBuilder()))
+        {
+            Console.SetOut(writer);
+
+            Std.input("Hello world!");
+
+            Assert.AreEqual("Hello world!", writer.ToString());
+        }
+    }
+}
